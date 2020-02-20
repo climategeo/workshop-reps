bc945066f0e176449011cd929d0b72a749415ead
Jihoon Jung <jjung5@uw.edu>
2020-02-20 15:38:09 -0800

    add functions

---
+++ functions.R
@@ -0,0 +1,15 @@
+
+
+challenge1 <- function (argument1, argument2){
+  
+  argument1 * argument2 / 100
+  
+}
+
+
+challenge2 <- function (argument1, argument2, argument3 = 100){
+  
+  argument1 * argument2 / argument3
+  
+}
+
